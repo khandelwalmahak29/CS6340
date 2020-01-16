30f15f525a9f26abf973cc5438575f8599bef1ee
CS6340 <you@mac.com>
2020-01-16 02:31:34 -0500

    add Instrument

--- fuzzing/part1/src/Instrument.cpp
+++ fuzzing/part1/src/Instrument.cpp
@@ -59,41 +59,37 @@ void instrumentSanitizer(Module *M, Function &F, Instruction &I) {
 void instrumentCoverage(Module *M, Function &F, Instruction &I) {
   /* Add your code here */
   //when running this instruction which has debug info, get instruction line and col, call coverage method
-  if(I.getDebugLoc()!= NULL) {
+  auto Debug = I.getDebugLoc();
+    if(Debug) {
 
-     outs() << "Coverage here " << "\n";
-      // //get variables
-      // auto Debug = I.getDebugLoc();
-      // auto num = I.getNumOperands();
-      // auto yValue = I.getOperand(0);
-      // auto xValue = I.getOperand(1);
-      // auto line = llvm::ConstantInt::get(llvm::Type::getInt32Ty(M->getContext()),Debug.getLine());
-      // auto col = llvm::ConstantInt::get(llvm::Type::getInt32Ty(M->getContext()),Debug.getCol());
+      //get variables
+      
+      auto line = llvm::ConstantInt::get(llvm::Type::getInt32Ty(M->getContext()),Debug.getLine());
+      auto col = llvm::ConstantInt::get(llvm::Type::getInt32Ty(M->getContext()),Debug.getCol());
         
-      // // Create the arguments vector from the my argument list
-      // SmallVector<Type *, 3> ArgTys; 
-      // ArgTys.push_back(Type::getInt32Ty(M->getContext())); 
-      // ArgTys.push_back(Type::getInt32Ty(M->getContext()));
-      // ArgTys.push_back(Type::getInt32Ty(M->getContext()));
+      // Create the arguments vector from the my argument list
+      SmallVector<Type *, 2> ArgTys; 
+      ArgTys.push_back(Type::getInt32Ty(M->getContext())); 
+      ArgTys.push_back(Type::getInt32Ty(M->getContext()));
+      
 
-      // // Just get a void return type
-      // Type *RetTy = Type::getVoidTy(M->getContext()); 
+      // Just get a void return type
+      Type *RetTy = Type::getVoidTy(M->getContext()); 
 
-      // // Create a new function with MyArgs as arguments
-      // Constant *C = M->getOrInsertFunction(SanitizerFunctionName, FunctionType::get(RetTy, ArgTys, false));
-      // Function* sanitizer = cast<Function>(C);
+      // Create a new function with MyArgs as arguments
+      Constant *C = M->getOrInsertFunction(CoverageFunctionName, FunctionType::get(RetTy, ArgTys, false));
+      Function* coverage = cast<Function>(C);
     
-      // SmallVector<Value*, 3> Args;
-      // Args.push_back(xValue);
-      // Args.push_back(line);
-      // Args.push_back(col);
+      SmallVector<Value*, 2> Args;
+      Args.push_back(line);
+      Args.push_back(col);
 
-      // //insert function sanitizer before instruction I
-      // //outs() << "called y value: " << F.getInstructionCount()<< "\n";
+      //insert function sanitizer before instruction I
+      //outs() << "called y value: " << F.getInstructionCount()<< "\n";
 
-      // CallInst *NewCall = CallInst::Create(sanitizer,Args, "", (Instruction *)(&I));
-      //   //outs() << "called y value: " << F.getInstructionCount()<< "\n";
-    }
+      CallInst *NewCall = CallInst::Create(coverage,Args, "", (Instruction *)(&I));
+        //outs() << "called y value: " << F.getInstructionCount()<< "\n";
+     }
 }
 
 bool Instrument::runOnFunction(Function &F) {
